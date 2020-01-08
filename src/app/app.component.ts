2f48ca167949b7635164fb53f296255fe01eea21
RiddermanR <roelof@ridderman.nl>
2020-01-08 16:12:36 +0100

    Chanced image locations to url's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,19 +20,19 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        '/assets/images/products/black-shoes.jpg',
+        'https://stackblitz.com/files/roelof-inventory/github/RiddermanR/roelof-inventory/master/src/assets/images/products/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '/assets/images/products/blue-jacket.jpg',
+        'https://stackblitz.com/files/roelof-inventory/github/RiddermanR/roelof-inventory/master/src/assets/images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '/assets/images/products/black-hat.jpg',
+        'https://stackblitz.com/files/roelof-inventory/github/RiddermanR/roelof-inventory/master/src/assets/images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
       ];
